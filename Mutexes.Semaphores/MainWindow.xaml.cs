52ae84abbf2abb29225f5ab76d737d3f183b11f9
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 13:04:27 +0300

    Ex 7 was done

--- Mutexes.Semaphores/MainWindow.xaml.cs
+++ Mutexes.Semaphores/MainWindow.xaml.cs
@@ -13,9 +13,53 @@ namespace Mutexes.Semaphores
 {
     public partial class MainWindow : Window
     {
+
+        private Mutex _mutex = new Mutex();
+        private SemaphoreSlim _semaphore = new SemaphoreSlim(1);
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void StartTask_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Text = ""; 
+
+            await Task.Run(() => ModifyArrayAndFindMax());
+        }
+
+        private void ModifyArrayAndFindMax()
+        {
+            int[] array = new int[10];
+
+            _mutex.WaitOne(); 
+            try
+            {
+                Random random = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(100); 
+                    AppendToOutput($"Modified array element {i}: {array[i]}");
+                    Thread.Sleep(100); 
+                }
+
+                int maxValue = array.Max();
+                AppendToOutput($"Maximum value in the array: {maxValue}");
+            }
+            finally
+            {
+                _mutex.ReleaseMutex(); 
+            }
+        }
+
+        private void AppendToOutput(string text)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                OutputTextBox.AppendText(text + Environment.NewLine);
+                OutputTextBox.ScrollToEnd(); 
+            });
         }
     }
 }
