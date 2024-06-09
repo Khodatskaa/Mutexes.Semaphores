fa8a928228abc6c3806d902271691490e02e9407
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 13:57:29 +0300

    Ex 1 was done

--- Mutexes.Semaphores/MainWindow.xaml.cs
+++ Mutexes.Semaphores/MainWindow.xaml.cs
@@ -13,9 +13,71 @@ namespace Mutexes.Semaphores
 {
     public partial class MainWindow : Window
     {
+
+        private Mutex _mutex = new Mutex();
+        private SemaphoreSlim _semaphore = new SemaphoreSlim(3); 
+        private Random _random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void StartThreads_Click(object sender, RoutedEventArgs e)
+        {
+            NumberListBox.Items.Clear(); 
+            StatusLabel.Content = "Threads running..."; 
+
+            await Task.WhenAll(
+                Task.Run(() => DisplayAscendingNumbers()),
+                Task.Run(() => DisplayDescendingNumbers())
+            );
+
+            StatusLabel.Content = "Threads completed."; 
+        }
+
+        private async Task DisplayAscendingNumbers()
+        {
+            _mutex.WaitOne(); 
+            try
+            {
+                for (int i = 0; i <= 20; i++)
+                {
+                    AppendToNumberListBox($"Thread 1: {i}");
+                    await Task.Delay(_random.Next(50, 200)); 
+                }
+            }
+            finally
+            {
+                _mutex.ReleaseMutex(); 
+            }
+        }
+
+        private async Task DisplayDescendingNumbers()
+        {
+            await _semaphore.WaitAsync(); 
+            try
+            {
+                await Task.Delay(100); 
+                for (int i = 10; i >= 0; i--)
+                {
+                    AppendToNumberListBox($"Thread 2: {i}");
+                    await Task.Delay(_random.Next(50, 200)); 
+                }
+            }
+            finally
+            {
+                _semaphore.Release(); 
+            }
+        }
+
+        private void AppendToNumberListBox(string text)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                NumberListBox.Items.Add(text);
+                NumberListBox.ScrollIntoView(text); 
+            });
         }
     }
 }
