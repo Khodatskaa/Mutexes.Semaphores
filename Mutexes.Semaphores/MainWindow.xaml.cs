2d560819b1b9b06bbbf78372fb1081785847df2e
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 13:02:38 +0300

    Ex 6 was done

--- Mutexes.Semaphores/MainWindow.xaml.cs
+++ Mutexes.Semaphores/MainWindow.xaml.cs
@@ -13,9 +13,67 @@ namespace Mutexes.Semaphores
 {
     public partial class MainWindow : Window
     {
+        private Mutex _mutex = new Mutex();
+        private SemaphoreSlim _semaphore = new SemaphoreSlim(3); 
+        private Random _random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void StartThreads_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Text = ""; 
+
+            await Task.WhenAll(
+                Task.Run(() => DisplayAscendingNumbers()),
+                Task.Run(() => DisplayDescendingNumbers())
+            );
+        }
+
+        private async Task DisplayAscendingNumbers()
+        {
+            _mutex.WaitOne(); 
+            try
+            {
+                for (int i = 0; i <= 20; i++)
+                {
+                    AppendToOutput($"Thread 1: {i}");
+                    await Task.Delay(_random.Next(50, 200)); 
+                }
+            }
+            finally
+            {
+                _mutex.ReleaseMutex(); 
+            }
+        }
+
+        private async Task DisplayDescendingNumbers()
+        {
+            await _semaphore.WaitAsync(); 
+            try
+            {
+                await Task.Delay(100); 
+                for (int i = 10; i >= 0; i--)
+                {
+                    AppendToOutput($"Thread 2: {i}");
+                    await Task.Delay(_random.Next(50, 200)); 
+                }
+            }
+            finally
+            {
+                _semaphore.Release(); 
+            }
+        }
+
+        private void AppendToOutput(string text)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                OutputTextBox.AppendText(text + Environment.NewLine);
+                OutputTextBox.ScrollToEnd(); 
+            });
         }
     }
 }
