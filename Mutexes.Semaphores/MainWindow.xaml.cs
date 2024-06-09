fbbbb138a2e462c947a96e74a04b8db048ec74d3
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 14:08:23 +0300

    Ex 3 was done

--- Mutexes.Semaphores/MainWindow.xaml.cs
+++ Mutexes.Semaphores/MainWindow.xaml.cs
@@ -1,21 +1,34 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Mutexes.Semaphores
 {
     public partial class MainWindow : Window
     {
+        private Semaphore semaphore;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            semaphore = new Semaphore(3, 3);
+
+            if (!semaphore.WaitOne(0))
+            {
+                MessageBox.Show("Only three instances of this module can run simultaneously.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            semaphore.Release();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button clicked!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
